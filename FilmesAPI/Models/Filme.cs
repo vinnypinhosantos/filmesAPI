3d671e22db7a6983f9e9d647da30e18f286b5888
Vinicius de Pinho Santos (CIEE) <vpinhosantos@ipt.br>
2023-04-10 14:12:13 -0300

    Criando Controller com método POST

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,31 @@
+﻿namespace FilmesAPI.Models;
+
+public class Filme
+{
+	private string _titulo;
+
+	public string Titulo
+	{
+		get { return _titulo; }
+		set { _titulo = value; }
+	}
+
+	private string _genero;
+
+	public string Genero
+	{
+		get { return _genero; }
+		set { _genero = value; }
+	}
+
+	private int _duracao;
+
+	public int Duracao
+	{
+		get { return _duracao; }
+		set { _duracao = value; }
+	}
+
+
+
+}
