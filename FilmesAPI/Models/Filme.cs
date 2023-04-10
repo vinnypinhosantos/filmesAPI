5cb695e32de118462a4464309e5e4697ec0b59f6
Vinicius de Pinho Santos (CIEE) <vpinhosantos@ipt.br>
2023-04-10 14:27:37 -0300

    Adicionando Data Annotations para restrições nos dados enviados pelo método POST

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,26 +1,33 @@
-﻿namespace FilmesAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
 
 public class Filme
 {
+	
 	private string _titulo;
-
-	public string Titulo
+    [Required(ErrorMessage = "O título do filme é obrigatório.")]
+    public string Titulo
 	{
 		get { return _titulo; }
 		set { _titulo = value; }
 	}
 
-	private string _genero;
 
-	public string Genero
+	private string _genero;
+    [Required(ErrorMessage = "O gênero do filme é obrigatório.")]
+	[MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres.")]
+    public string Genero
 	{
 		get { return _genero; }
 		set { _genero = value; }
 	}
 
-	private int _duracao;
 
-	public int Duracao
+	private int _duracao;
+    [Required]
+	[Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
+    public int Duracao
 	{
 		get { return _duracao; }
 		set { _duracao = value; }
